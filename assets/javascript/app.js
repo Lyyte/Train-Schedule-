7f1a653119a06a7a476bf948c2e5e07a03777422
Lyyte <lyytefaridi@gmail.com>
2020-03-11 18:55:44 -0500

    set up files

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyBb3pWojzBMwl_9m1e7be0fzA45pLceDPQ",
+        authDomain: "train-schedule-hw-94645.firebaseapp.com",
+        databaseURL: "https://train-schedule-hw-94645.firebaseio.com",
+        projectId: "train-schedule-hw-94645",
+        storageBucket: "train-schedule-hw-94645.appspot.com",
+        messagingSenderId: "264902581767",
+        appId: "1:264902581767:web:1b8e652a0e2e15fbb294a2",
+        measurementId: "G-GWYRYBQ6JW"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+});
