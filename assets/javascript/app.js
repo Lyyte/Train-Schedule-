3718b44f53686dd70eeff1d5020593ab7fdc8213
Lyyte <lyytefaridi@gmail.com>
2020-03-12 16:48:37 -0500

    added test column, added form submissions, getting denied firebase permisssion

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,47 @@
-// $( document ).ready(function() {
-//     console.log( "ready!" );
-
-//     var firebaseConfig = {
-//         apiKey: "AIzaSyBb3pWojzBMwl_9m1e7be0fzA45pLceDPQ",
-//         authDomain: "train-schedule-hw-94645.firebaseapp.com",
-//         databaseURL: "https://train-schedule-hw-94645.firebaseio.com",
-//         projectId: "train-schedule-hw-94645",
-//         storageBucket: "train-schedule-hw-94645.appspot.com",
-//         messagingSenderId: "264902581767",
-//         appId: "1:264902581767:web:1b8e652a0e2e15fbb294a2",
-//         measurementId: "G-GWYRYBQ6JW"
-//       };
-//       // Initialize Firebase
-//       firebase.initializeApp(firebaseConfig);
-// });
 console.log("hello");
-
 function displayTime() {
     var time = moment().format('HH:mm:ss');
     $('#clock').html("Current Time: " + time + " CT");
     setTimeout(displayTime, 1000);
-}
-
+};
 $(document).ready(function() {
     displayTime();
-});
+    var config = {
+    apiKey: "AIzaSyBb3pWojzBMwl_9m1e7be0fzA45pLceDPQ",
+    authDomain: "train-schedule-hw-94645.firebaseapp.com",
+    databaseURL: "https://train-schedule-hw-94645.firebaseio.com",
+    storageBucket: "train-schedule-hw-94645.appspot.com",
+  };
+  
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $('#add-train').on("click", function(event) {
+    event.preventDefault();
+    var trainname = $('#train-name-input').val().trim();
+    var destination = $('#destination-input').val().trim();
+    var first = $('#first-input').val().trim();
+    var freq = $('#frequency-input').val().trim();
+
+    var traininfo = {
+        name: trainname,
+        destin: destination,
+        firsttrain: first,
+        frequency: freq,
+    }
+
+    database.ref().push(traininfo);
+
+    console.log(traininfo.name);
+    console.log(traininfo.destin);
+
+
+    // var newRow = $("<tr>").append(
+    //     $("<td>").text(trainname),
+    //     $("<td>").text(destination),
+    //     $("<td>").text(first),
+    //     $("<td>").text(freq),
+
+  });
+  });
